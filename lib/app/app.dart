0a26a6774143ea5ab60539b2437230b484f2e8ad
thevzurd <thevzurd@gmail.com>
2019-10-27 14:58:09 -0400

    Revert "changes to introdules a globals class for app wide access"
    This reverts commit de08569d1f17d0557500c71ce6d9c6bb229806d0.

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,12 +1,15 @@
 import 'dart:async';
+import 'package:flutter/material.dart';
+import '../app/widgets/splash_screen.dart';
+
 export 'bloc/data.dart';
 export 'services/auth_user_service.dart';
-export 'services/globals.dart' show globals;
 export 'services/network.dart';
 export 'services/services_provider.dart';
 export 'services/storage.dart';
 export 'services/user.dart';
 export 'utils/utils.dart';
+export 'widgets/splash_screen.dart';
 export 'widgets/winwisely99.dart';
 
 Completer<dynamic> storageServiceReadyCompleter = Completer<dynamic>();
@@ -17,3 +20,4 @@ Future<dynamic> get storageReady => storageServiceReadyCompleter.future;
 Future<dynamic> get networkReady => networkServiceReadyCompleter.future;
 Future<dynamic> get userServiceReady => userServiceReadyCompleter.future;
 Future<dynamic> get authUserReady => userServiceReadyCompleter.future;
+final Map<String, Widget> navigationMap = <String, Widget>{};
--- lib/app/services/navigation.dart
+++ lib/app/services/navigation.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+// TODO(FlutterDevelopers): Import modules here
 import '../app.dart';
 
 Route<dynamic> routes(RouteSettings settings) {
+  navigationMap['/'] = const SplashScreen(
+    key: ValueKey<String>('/'),
+  );
   return MaterialPageRoute<dynamic>(
     builder: (BuildContext context) {
-      return globals.navigationMap[settings.name];
+      return navigationMap[settings.name];
     },
   );
 }
